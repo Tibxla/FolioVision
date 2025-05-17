c2fcfeaab854e90d687abd15c260cb066a7387ee
Tibxla <160187533+Tibxla@users.noreply.github.com>
2025-05-17 19:45:44 +0200

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,3 +1,7 @@
+DROP DATABASE IF EXISTS foliovision;
+CREATE DATABASE foliovision;
+USE foliovision;
+
 CREATE TABLE users (
                        user_id INT AUTO_INCREMENT PRIMARY KEY,
                        username VARCHAR(50) NOT NULL UNIQUE,
